df5fc8bfec2fe7a94fc38585062fcbeeec1de45a
Eric <ericmuthuipatch22@gmail.com>
2023-07-23 15:24:50 +0300

    added transaction form and list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TransactionList from './components/TransactionList';
+import TransactionForm from './components/TransactionForm';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3000/transactions')
+      .then((response) => response.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleFormSubmit = (formData) => {
+    setTransactions((prevTransactions) => [
+      ...prevTransactions,
+      {
+        id: Date.now(), // This will create a unique ID for the new transaction (temporary)
+        ...formData,
+      },
+    ]);
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Transactions</h1>
+      <TransactionForm onSubmit={handleFormSubmit} />
+      <div>
+        <label>Search:</label>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search by description..."
+        />
+      </div>
+      <TransactionList transactions={filteredTransactions} />
     </div>
   );
 }
